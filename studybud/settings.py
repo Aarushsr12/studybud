575f4893cf04f070360458a3b46120ad3a947db5
Aarushsr12 <aarushsrivastava1212@gmail.com>
2024-07-06 00:59:42 +0530

    Connected Postgresql db

--- studybud/settings.py
+++ studybud/settings.py
@@ -76,10 +76,16 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# POSTGRES DATABASE CONNECTION SETUP
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
