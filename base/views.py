2be8fdae371d6543c0d6d497f7f55a64960ebd05
Aarushsr12 <aarushsrivastava1212@gmail.com>
2024-07-06 14:27:44 +0530

    Topic, message model added

--- base/views.py
+++ base/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Room
 # Create your views here.
 # Dump Business logic 
 
-rooms = [
-    {'id':1, 'name': 'learn python with me'},
-    {'id':2, 'name': 'Backend Devs'},
-    {'id':3, 'name': 'frontend devs'},
-]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request,'base/home.html', context )
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    #unpack error encountered here, pass it like id=pk
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
